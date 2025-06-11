c0b84d97d9e1bce6ea6a67534eeaa2e4a22a81d2
alunoEtec <aula@etec.com>
2025-06-10 22:04:26 -0300

    css feito

--- pokedex/script.js
+++ pokedex/script.js
@@ -2,6 +2,27 @@ function Clicar() {
     var input = document.getElementById('pokemon').value.trim();
     var pokemonInfo = document.getElementById('pokemon-info');
 
+    const tiposPT = {
+        normal: "ormal",
+        fire: "fogo",
+        water: "água",
+        grass: "grama",
+        electric: "elétrico",
+        ice: "gelo",
+        fighting: "lutador",
+        poison: "veneno",
+        ground: "terra",
+        flying: "voador",
+        psychic: "psíquico",
+        bug: "inseto",
+        rock: "pedra",
+        ghost: "fantasma",
+        dragon: "dragão",
+        dark: "noturno",
+        steel: "aço",
+        fairy: "fada"
+      };
+      
     if (!input) {
         alert("Por favor, insira o nome ou número do Pokémon.");
         return;
@@ -20,7 +41,10 @@ function Clicar() {
             document.getElementById('name').textContent = data.name;
             document.getElementById('num').textContent = data.order;
             document.getElementById('imagem').src = data.sprites.other['official-artwork'].front_default;
-            document.getElementById('tipo').textContent = data.types[0].type.name;
+            document.getElementById('tipo').textContent = tipoIngles = data.types[0].type.name;
+
+            const tipoTraduzido = tiposPT[tipoIngles] || tipoIngles;
+            tipo.textContent = "tipo " + tipoTraduzido;
 
             // torna a div visível
             pokemonInfo.style.display = 'flex'; // ou 'block', dependendo do layout desejado
